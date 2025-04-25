9916d9100c57919cb52daf6895c18bd1026fb656
ABuljko <ajdin.buljko17@gmail.com>
2025-04-25 10:29:07 +0200

    backendfinished

--- backend/index.js
+++ backend/index.js
@@ -27,7 +27,7 @@ app.get('/', (req, res) => {
     res.json({ data: "Hello from server" });
 });
 
-app.post("./create-account", async (req, res) => {
+app.post("/create-account", async (req, res) => {
     const { fullName, email, password } = req.body;
     
     if(!fullName) {
@@ -63,7 +63,7 @@ app.post("./create-account", async (req, res) => {
     });
 });
 
-app.post("./login", async (req, res) => {
+app.post("/login", async (req, res) => {
     const { email, password } = req.body;
     
     if(!email) {
@@ -79,7 +79,8 @@ app.post("./login", async (req, res) => {
         return res.status(400).json({ error: true, message:"User not found" });
     }
     if(userInfo.email==email && userInfo.password==password) {
-        const user=jwt.sign(userInfo,process.env.ACCESS_TOKEN_SECRET,{
+        const{ user } = userInfo;
+        const accessToken=jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{
             expiresIn: '36000m'
         });
         return res.json({
@@ -94,11 +95,29 @@ app.post("./login", async (req, res) => {
 
 });
 
+app.get("/get-user",authenticateToken , async (req, res) => {
+
+    const {user} = req.user;
+
+    const isUser = await User.findOne({ _id:user._id });
+    if(!isUser) {
+        return res.sendStatus(401);
+    }
+
+    return res.json({
+        user:{fullName: isUser.fullName, email: isUser.email, "_id": isUser._id, createdOn: isUser.createdOn},
+        message: "",
+    });
+});
+
 app.post("/add-note", authenticateToken, async (req, res) => {
-    if(title){
+    const { title, content, tags } = req.body;
+    const { user } = req.user;
+
+    if(!title){
         return res.status(400).json({ error: true, message:"Title is required" });
     }
-    if(content){
+    if(!content){
         return res.status(400).json({ error: true, message:"Content is required" });
     }
     try {
@@ -120,7 +139,7 @@ app.post("/add-note", authenticateToken, async (req, res) => {
     }
 });
 
-app.post("/edit-note/:noteId", authenticateToken, async (req, res) => {
+app.put("/edit-note/:noteId", authenticateToken, async (req, res) => {
     const noteId = req.params.noteId;
     const { title, content, tags, isPinned } = req.body;
     const{user}=req.user;
@@ -161,7 +180,7 @@ catch (error) {
 }
 });
 
-app.post("/get-all-notes/", authenticateToken, async (req, res) => {
+app.get("/get-all-notes/", authenticateToken, async (req, res) => {
 
     const { user } = req.user;
 
@@ -171,6 +190,7 @@ app.post("/get-all-notes/", authenticateToken, async (req, res) => {
 
         return res.json({
             error: false,
+            notes,
             message: "Notes fetched successfully",
         });
     } catch (error) {
@@ -178,7 +198,7 @@ app.post("/get-all-notes/", authenticateToken, async (req, res) => {
     }
 });
 
-app.post("/delete-note/:noteId", authenticateToken, async (req, res) => {
+app.delete("/delete-note/:noteId", authenticateToken, async (req, res) => {
 
     const noteId = req.params.noteId;
     const { user } = req.user;
@@ -197,6 +217,33 @@ app.post("/delete-note/:noteId", authenticateToken, async (req, res) => {
     }catch (error) {
         return res.status(500).json({ error: true, message: "Internal server error" });
     }
+});
+
+app.put("/update-note-pinned/:noteId", authenticateToken, async (req, res) => {
+    const noteId = req.params.noteId;
+    const { isPinned } = req.body;
+    const{user}=req.user;
+
+    try {
+    const note = await Note.findOne({ _id: noteId, userId: user._id });
+
+    if (!note) {
+        return res.status(404).json({ error: true, message:"Note not found" });
+    }
+
+        note.isPinned = isPinned;	
+
+
+    await note.save();
+    return res.json({
+        error: false,
+        note,
+        message: "Note updated successfully",
+    });
+}
+catch (error) {
+    return res.status(500).json({ error: true, message: "Internal server error" });
+}
 });
 
 app.listen( 8000);
